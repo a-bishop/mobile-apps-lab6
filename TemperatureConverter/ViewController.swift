511d0a56ec85a36bc53f6690193ce445160e3eae
Andrew Bishop <andrew.bishop53@gmail.com>
2019-01-18 14:54:48 -0800

    Initial Commit

---
+++ TemperatureConverter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TemperatureConverter
+//
+//  Created by Andrew on 2019-01-18.
+//  Copyright © 2019 ICS224. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

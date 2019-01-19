e3fa3d8afdeaad72b315636f475fb38534fc5de0
Andrew Bishop <andrew.bishop53@gmail.com>
2019-01-18 17:00:15 -0800

    Finished requirements for Lab 2

--- TemperatureConverter/ViewController.swift
+++ TemperatureConverter/ViewController.swift
@@ -8,13 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    // MARK: Properties
+    
+    @IBOutlet weak var tempField: UITextField!
+    @IBOutlet weak var tempResult: UILabel!
+    
+    // MARK: Delegate Methods
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.tempField.placeholder = "Enter a temperature"
+        self.tempResult.textAlignment = .center
+        self.tempField.delegate = self
     }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.tempField.resignFirstResponder()
+        return true
+    }
+    
+    // MARK: Actions
 
-
+    @IBAction func toF(_ sender: Any) {
+        // Check if user input String can be converted to Double
+        if let userTemp = Double(self.tempField.text!) {
+            // Conversion formula
+            let convertedTemp = userTemp * 9.0 / 5.0 + 32.0
+            // Round the result to at most three decimal places
+            let roundConvertedTemp = Double(round(1000 * convertedTemp) / 1000)
+            self.tempResult.text = String(roundConvertedTemp)
+        } else {
+            self.tempResult.text = "N/A"
+        }
+    }
+    @IBAction func toC(_ sender: Any) {
+        // Check if user input String can be converted to Double
+        if let userTemp = Double(self.tempField.text!) {
+            // Conversion formula
+            let convertedTemp = (userTemp - 32.0) * 5.0 / 9.0
+            // Round the result to at most three decimal places
+            let roundConvertedTemp = Double(round(1000 * convertedTemp) / 1000)
+            self.tempResult.text = String(roundConvertedTemp)
+        } else {
+            self.tempResult.text = "N/A"
+        }
+    }
+    
 }
 
